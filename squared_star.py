655de4cac84b3d53d7b4a30377a19f23e56e8419
Charlie G <charlieg895@gmail.com>
2017-07-26 13:35:50 -0400

    added header comments

--- squared_star.py
+++ squared_star.py
@@ -1,3 +1,10 @@
+"""
+Creates a star using a four-square figure.
+Hard-coded to draw the four-square 5 times, equally rotated along 180 degrees. 
+Only certain numbers guarantee non-reptition of four-square being drawn. 
+Math could probably figure which could reduce the number of figures necessary. 
+"""
+
 import turtle as t
 
 def line(length, turt):
